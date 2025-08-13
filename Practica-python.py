8899fea03a6a7858527083d113ad6300daa44557
paul el abusador <madepaul17@gmail.com>
2025-08-12 13:06:13 -1200

    ejericio 2

--- Practica-python.py
+++ Practica-python.py
@@ -12,7 +12,8 @@ print("hola perros " +  str(nombre))
 # --------------------------------
 # Ejercicio 2: Edad en un mensaje
 # --------------------------------
-
+nombre = input("ingresar nombre")
+print("hola perros " +  str(nombre))
 # --------------------------------
 # Ejercicio 3: Nombre y edad juntos
 # --------------------------------
