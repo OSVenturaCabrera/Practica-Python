661332fa61c147d6190725d4dfd4d674aa2845a7
paul el abusador <madepaul17@gmail.com>
2025-08-12 13:00:00 -1200

    ejercicio de carlos montequiu

--- Practica-python.py
+++ Practica-python.py
@@ -7,7 +7,8 @@
 # Ejercicio 1: Hola nombre
 # Pide el nombre al usuario y lo muestra con un saludo
 # --------------------------------
-
+nombre = input("ingresar nombre")
+print("hola perros " +  str(nombre))
 # --------------------------------
 # Ejercicio 2: Edad en un mensaje
 # --------------------------------
